d0240451ba1cd6f359201d2232915feb7af6af43
Have Samuel <samiehave9@gmail.com>
2022-03-22 04:09:39 +0300

    Fix Linters

--- modules/date.js
+++ modules/date.js
@@ -1,4 +1,4 @@
-import { DateTime } from '/node_modules/luxon/src/luxon.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
 
 const dayTime = document.querySelector('#date');
 dayTime.innerText = DateTime.now().toFormat('MMMM dd, yyyy');
